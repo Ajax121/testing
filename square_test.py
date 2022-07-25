1102062d755423d0d265b38997430e576db574a1
Arjun Haridas <arjunharidas@Arjuns-MacBook-Pro.local>
2022-07-25 13:59:57 +0200

    sd

--- square_test.py
+++ square_test.py
@@ -1,30 +1,37 @@
-import imp
+"""
+asd
+"""
 import pytest
 from square import squ
-import warnings
-"""
-unit test working with classes
-
-unit test - test an isolated unit of code
-integration test - test the working interaction of ur code
-
-test file needs to be saved as filename_test.py
-
-tests
------
-unit tests / integration test / regression test / acceptance test
-"""
 
 def test_out():
+    """
+    unit test working with classes
+
+    unit test - test an isolated unit of code
+    integration test - test the working interaction of ur code
+
+    test file needs to be saved as filename_test.py
+
+    tests
+    -----
+    unit tests / integration test / regression test / acceptance test
+    """
     assert squ(2)==4
 
 def test_out_type():
-    assert type(squ(2))==int
+    """_summary_
+    """
+    assert isinstance(squ(2),int)  #type(squ(2))==int
 
 def test_inp_error():
+    """_summary_
+    """
     with pytest.raises(TypeError):
         squ('meep')
 
 def test_warning_float():
+    """_summary_
+    """
     with pytest.warns(UserWarning):
         squ(3.0)
